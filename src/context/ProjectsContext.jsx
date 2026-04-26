b364b04be9163db99051fdc605d7401c9cb64543
tao544 <taoheedadepoju1@gmail.com>
2026-04-26 19:24:40 +0100

    fix duplicate useProjects export

--- src/context/ProjectsContext.jsx
+++ src/context/ProjectsContext.jsx
@@ -107,6 +107,11 @@ export function ProjectsProvider({ children }) {
   )
 }
 
+
 export function useProjects() {
-  return useContext(ProjectsContext)
+  const context = useContext(ProjectsContext)
+  if (!context) {
+    throw new Error('useProjects must be used within a ProjectsProvider')
+  }
+  return context
 }
